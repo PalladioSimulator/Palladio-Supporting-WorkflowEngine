2acb914218c893efcc10e541aa4459b0e6c26d74
BenjaminKlatt <klatt@fzi.de>
2010-07-28 12:19:40 +0000

    PCMResourceSetPartition.getElement(EClass) adopted to not throw an exception for empty model resources but simply ignore them. Fixed a potential null pointer exception, too

--- de.uka.ipd.sdq.workflow.pcm/src/de/uka/ipd/sdq/workflow/pcm/blackboard/PCMResourceSetPartition.java
+++ de.uka.ipd.sdq.workflow.pcm/src/de/uka/ipd/sdq/workflow/pcm/blackboard/PCMResourceSetPartition.java
@@ -112,10 +112,7 @@ public class PCMResourceSetPartition extends ResourceSetPartition {
 	private List<EObject> getElement(EClass clazz) {
 		ArrayList<EObject> result = new ArrayList<EObject>();
 		for (Resource r : this.rs.getResources()) {
-			if (r == null){
-				throw new RuntimeException("Failed to retrieve PCM model element "+clazz.getName()+", encountered invalid resource "+r.getURI());
-			}
-			if (r.getContents().size() > 0 && r.getContents().get(0).eClass() == clazz ) {
+			if (r != null && r.getContents().size() > 0 && r.getContents().get(0).eClass() == clazz ) {
 				result.add(r.getContents().get(0));
 			}
 		}

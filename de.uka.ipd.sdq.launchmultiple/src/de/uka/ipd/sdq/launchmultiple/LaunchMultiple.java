81e141df0e038db2b592e99fe69888bec8bbe388
AnneKoziolek <anne.koziolek@kit.edu>
2011-03-17 13:59:38 +0000

    show error message in launch multiple if the run configuartion has not been loaded before.

--- de.uka.ipd.sdq.launchmultiple/src/de/uka/ipd/sdq/launchmultiple/LaunchMultiple.java
+++ de.uka.ipd.sdq.launchmultiple/src/de/uka/ipd/sdq/launchmultiple/LaunchMultiple.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
@@ -28,39 +29,45 @@ public class LaunchMultiple implements ILaunchConfigurationDelegate {
 	 */
 	public void launch(ILaunchConfiguration configuration, String mode,
 			ILaunch launch, IProgressMonitor monitor) throws CoreException {
-		
+
 		//FIXME: Add a better way to access this
 		LaunchMultipleTab tab = LaunchMultipleTab.getAnyInstance();
-		
-		List<ILaunchConfiguration> configs = tab.getLaunchConfigs();
-		
-		List<ILaunchConfiguration> configsToBeLaunched = new ArrayList<ILaunchConfiguration>();
-		
-		for (ILaunchConfiguration launchConfiguration : configs) {
-			boolean toBeLaunched = configuration.getAttribute(launchConfiguration.getName(), false);
-			if (toBeLaunched){
-				configsToBeLaunched.add(launchConfiguration);
+
+		if (tab != null){
+
+			List<ILaunchConfiguration> configs = tab.getLaunchConfigs();
+
+			List<ILaunchConfiguration> configsToBeLaunched = new ArrayList<ILaunchConfiguration>();
+
+			for (ILaunchConfiguration launchConfiguration : configs) {
+				boolean toBeLaunched = configuration.getAttribute(launchConfiguration.getName(), false);
+				if (toBeLaunched){
+					configsToBeLaunched.add(launchConfiguration);
+				}
 			}
-		}
-		
-		for (ILaunchConfiguration launchConfiguration : configsToBeLaunched) {
-			
-			try {
-			
-			//retrieve SimuCom Launch Delegate
-			Set<String> modes = new HashSet<String>();
-			modes.add(mode);
-			ILaunchConfigurationDelegate delegate = launchConfiguration.getType().getDelegates(modes)[0].getDelegate();
-			delegate.launch(launchConfiguration, mode, launch, monitor);
-			
-			} catch (Exception e){
-				//Run all launches, whatever happens. Thus catch all exceptions, print something, then continue.
-				logger.error("Running "+launchConfiguration.getName()+" failed. I will start the next one. Cause: "+e.getMessage());
-				e.printStackTrace();
+
+			for (ILaunchConfiguration launchConfiguration : configsToBeLaunched) {
+
+				try {
+
+					//retrieve SimuCom Launch Delegate
+					Set<String> modes = new HashSet<String>();
+					modes.add(mode);
+					ILaunchConfigurationDelegate delegate = launchConfiguration.getType().getDelegates(modes)[0].getDelegate();
+					delegate.launch(launchConfiguration, mode, launch, monitor);
+
+				} catch (Exception e){
+					//Run all launches, whatever happens. Thus catch all exceptions, print something, then continue.
+					logger.error("Running "+launchConfiguration.getName()+" failed. I will start the next one. Cause: "+e.getMessage());
+					e.printStackTrace();
+				}
+
 			}
-			
+
+		} else {
+			throw new CoreException(new Status(Status.ERROR, this.getClass().getPackage().getName(), "Cannot run launmch multiple before run config has been opened once to initialise tabs. Please start this using the run configuration menu entry."));
 		}
-		
+
 	}
 
 }

2e21f635d0d711fc9fd02a7d86e2998f7366b46e
mhauck <hauck@fzi.de>
2011-08-17 08:55:39 +0000

    Move de.uka.ipd.sdq.errorhandling to WorkflowEngine

---
+++ de.uka.ipd.sdq.errorhandling/src/de/uka/ipd/sdq/errorhandling/IIssue.java
@@ -0,0 +1,10 @@
+package de.uka.ipd.sdq.errorhandling;
+
+/**Interface for reportable issues.
+ * @author groenda
+ *
+ */
+public interface IIssue {
+	/** Issue message. */
+	public String getMessage();
+}

d8f630930b214fa05dd49a560f393a651708da75
mhauck <hauck@fzi.de>
2012-02-14 14:44:20 +0000

    Added AbstractMDSDWorkbenchDelegate for adding MDSDBlackboard

---
+++ de.uka.ipd.sdq.workflow.mdsd/src/de/uka/ipd/sdq/workflow/mdsd/AbstractMDSDWorkbenchDelegate.java
@@ -0,0 +1,34 @@
+package de.uka.ipd.sdq.workflow.mdsd;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+
+import de.uka.ipd.sdq.workflow.AbstractJobConfiguration;
+import de.uka.ipd.sdq.workflow.BlackboardBasedWorkflow;
+import de.uka.ipd.sdq.workflow.Workflow;
+import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
+import de.uka.ipd.sdq.workflow.workbench.AbstractWorkbenchDelegate;
+
+public abstract class AbstractMDSDWorkbenchDelegate<WorkflowConfigurationType extends AbstractJobConfiguration, WorkflowType extends Workflow>
+		extends AbstractWorkbenchDelegate<WorkflowConfigurationType, WorkflowType> {
+
+	/**
+	 * Instantiate the workflow engine. By default a standard workflow engine is
+	 * created.
+	 * 
+	 * @param workflowConfiguration
+	 *            Configuration of the workflow job
+	 * @return The workflow engine to use for this launch
+	 * @throws CoreException
+	 */
+	@SuppressWarnings("unchecked")
+	protected WorkflowType createWorkflow(
+			WorkflowConfigurationType workflowConfiguration) {
+		return (WorkflowType) new BlackboardBasedWorkflow<MDSDBlackboard>(
+				createWorkflowJob(workflowConfiguration),
+				new NullProgressMonitor(),
+				// createExceptionHandler(workflowConfiguration.isInteractive()));
+				createExceptionHandler(true), new MDSDBlackboard());
+	}
+
+}

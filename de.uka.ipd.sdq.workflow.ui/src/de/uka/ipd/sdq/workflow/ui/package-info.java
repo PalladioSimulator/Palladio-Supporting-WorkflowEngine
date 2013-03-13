37ebacf52d5d03c3c6064784de84e17571dde82f
BenjaminKlatt <klatt@fzi.de>
2013-03-13 21:14:27 +0000

    WFE-40 Move Workflow core ui package to workflow.ui plugin

--- de.uka.ipd.sdq.workflow.ui/src/de/uka/ipd/sdq/workflow/ui/package-info.java
+++ de.uka.ipd.sdq.workflow.ui/src/de/uka/ipd/sdq/workflow/ui/package-info.java
@@ -1,8 +1,7 @@
 /**
  * The ui package of the workflow engine.
  * 
- * This package provides an integration with the progress monitoring 
- * of the Eclipse IDE.
+ * Workflow infrastructure integrated with the UI of the Eclipse IDE.
  *
  * @author Palladio Project
  * @version 1.0

a8553cdb67e8886ddb64201521bc8dbe011b55e2
BenjaminKlatt <klatt@fzi.de>
2013-09-27 14:22:47 +0000

    WFE-54 Release Version 2.0.2

---
+++ de.uka.ipd.sdq.workflow.workbench/src/de/uka/ipd/sdq/workflow/workbench/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The workbench package of the workflow engine.
+ * 
+ * This package provides an integration with the Eclipse workbench.
+ *
+ * @author Palladio Project
+ * @version 1.0
+ *
+*/
+package de.uka.ipd.sdq.workflow.workbench;

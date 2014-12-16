ba75a27c4c43e32299ba652421cc2be2c5fcf932
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-12-16 15:10:19 +0000

    Imprved inheritance hierarchy; did not make reuse of already implemented blackboard class before

--- de.uka.ipd.sdq.workflow/src/de/uka/ipd/sdq/workflow/extension/AbstractWorkflowExtensionJob.java
+++ de.uka.ipd.sdq.workflow/src/de/uka/ipd/sdq/workflow/extension/AbstractWorkflowExtensionJob.java
@@ -1,61 +1,23 @@
 package de.uka.ipd.sdq.workflow.extension;
 
 import de.uka.ipd.sdq.workflow.blackboard.Blackboard;
-import de.uka.ipd.sdq.workflow.jobs.IBlackboardInteractingJob;
-import de.uka.ipd.sdq.workflow.jobs.SequentialJob;
+import de.uka.ipd.sdq.workflow.jobs.SequentialBlackboardInteractingJob;
 
 /**
- * Abstract extension job. This class specifies jobs which can be plugged in into an extendible
- * work flow as provided by the work flow engine.
+ * Abstract extension job. This class specifies jobs which can be plugged in into an extendible work
+ * flow as provided by the work flow engine.
  * 
  * @param <BlackboardType>
  *            the generic type
  * @author Benjamin Klatt
  * @author Michael Hauck
+ * @author Sebastian Lehrig
  */
 public abstract class AbstractWorkflowExtensionJob<BlackboardType extends Blackboard<?>> extends
-        SequentialJob implements IBlackboardInteractingJob<BlackboardType> {
-
-    // ////////////////////////////////
-    // ATTRIBUTES
-    // ////////////////////////////////
+        SequentialBlackboardInteractingJob<BlackboardType> {
 
     /** The configuration of this job extension. */
     private AbstractExtensionJobConfiguration jobConfiguration = null;
-
-    /** The blackboard accessible for this job. */
-    protected BlackboardType blackboard;
-
-    // ////////////////////////////////
-    // CONSTRUCTORS
-    // ////////////////////////////////
-
-    // ////////////////////////////////
-    // BUSINESS METHODS
-    // ////////////////////////////////
-
-    // ////////////////////////////////
-    // GETTER / SETTER
-    // ////////////////////////////////
-
-    /**
-     * Get the blackboard to work with.
-     * 
-     * @return the blackboard
-     */
-    public BlackboardType getMyBlackboard() {
-        return blackboard;
-    }
-
-    /**
-     * Set the blackboard to work with.
-     * 
-     * @param blackboard
-     *            the blackboard to set
-     */
-    public void setBlackboard(BlackboardType blackboard) {
-        this.blackboard = blackboard;
-    }
 
     /**
      * Get the configuration of this job.
@@ -74,18 +36,5 @@ public abstract class AbstractWorkflowExtensionJob<BlackboardType extends Blackb
      */
     public void setJobConfiguration(AbstractExtensionJobConfiguration jobConfiguration) {
         this.jobConfiguration = jobConfiguration;
-    }
-
-    /**
-     * Set the blackboard for the extension job. A type cast is required by the method due to the
-     * fact that the general extension point is not able to determine the blackboard type only
-     * available at runtime.
-     * 
-     * @param blackboard
-     *            The blackboard to set.
-     */
-    @SuppressWarnings("unchecked")
-    public void setJobExtensionBlackboard(Blackboard<?> blackboard) {
-        this.blackboard = (BlackboardType) blackboard;
     }
 }

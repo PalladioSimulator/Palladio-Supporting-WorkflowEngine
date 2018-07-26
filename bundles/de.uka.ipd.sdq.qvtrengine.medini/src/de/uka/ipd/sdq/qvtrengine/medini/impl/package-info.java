e7814c732dd48cff68c0d67d7fac453eccb3c73b
Stephan Seifermann <seifermann@fzi.de>
2018-07-26 13:51:01 +0200

    WFE-72 Removed QVT-R plugins.

--- bundles/de.uka.ipd.sdq.qvtrengine.medini/src/de/uka/ipd/sdq/qvtrengine/medini/impl/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @author Palladio Project
- * @version 1.0
- *
-*/
-package de.uka.ipd.sdq.qvtrengine.medini.impl;
--- bundles/de.uka.ipd.sdq.workflow.mdsd/src/de/uka/ipd/sdq/workflow/mdsd/emf/qvtr/oaw/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @author Palladio Project
- * @version 1.0
- *
- */
-package de.uka.ipd.sdq.workflow.mdsd.emf.qvtr.oaw;

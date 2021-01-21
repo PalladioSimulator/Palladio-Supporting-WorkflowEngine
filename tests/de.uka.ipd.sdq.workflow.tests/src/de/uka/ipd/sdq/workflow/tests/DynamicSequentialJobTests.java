c98f2948f99ef22927d10b5524aec2b47163a27b
Sebastian Krach <krach@fzi.de>
2021-01-21 13:30:02 +0100

    Add tests to keep up coverage

---
+++ tests/de.uka.ipd.sdq.workflow.tests/src/de/uka/ipd/sdq/workflow/tests/DynamicSequentialJobTests.java
@@ -0,0 +1,202 @@
+package de.uka.ipd.sdq.workflow.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedList;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uka.ipd.sdq.workflow.WorkflowFailedException;
+import de.uka.ipd.sdq.workflow.jobs.CleanupFailedException;
+import de.uka.ipd.sdq.workflow.jobs.DynamicSequentialJob;
+import de.uka.ipd.sdq.workflow.jobs.IJob;
+import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
+import de.uka.ipd.sdq.workflow.jobs.SequentialJob;
+import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
+import de.uka.ipd.sdq.workflow.mocks.CancelingJob;
+import de.uka.ipd.sdq.workflow.mocks.FailingJob;
+import de.uka.ipd.sdq.workflow.mocks.MockJob;
+
+/**
+ * The Class SequentialJobTests.
+ */
+public class DynamicSequentialJobTests {
+
+    /** The my comp job. */
+    private SequentialJob myCompJob = null;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see junit.framework.TestCase#setUp()
+     */
+    @Before
+    public void setUp() {
+        myCompJob = new DynamicSequentialJob();
+        MockJob.resetExecutionNumber();
+    }
+
+    /**
+     * add a job. execute the composite job. check if the added job was executed. cleanup the
+     * composite job. check if the added job was cleaned up.
+     *
+     * @throws JobFailedException
+     *             the job failed exception
+     * @throws UserCanceledException
+     *             the user canceled exception
+     * @throws CleanupFailedException
+     *             the cleanup failed exception
+     */
+    @Test
+    public void testJobHandling() throws JobFailedException, UserCanceledException, CleanupFailedException {
+        final MockJob job = new MockJob();
+        myCompJob.addJob(job);
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+
+        myCompJob.execute(monitor);
+        assertEquals(true, job.wasExecuted());
+
+        myCompJob.cleanup(monitor);
+        assertEquals(true, job.wasCleanedUp());
+    }
+
+    /**
+     * add a number of jobs. execute the composite job. check if all jobs were executed in the order
+     * they were added.
+     *
+     * @throws JobFailedException
+     *             the job failed exception
+     * @throws UserCanceledException
+     *             the user canceled exception
+     */
+    @Test
+    public void testInOrderExecution() throws JobFailedException, UserCanceledException {
+        final LinkedList<MockJob> jobs = new LinkedList<MockJob>();
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+
+        for (int i = 0; i < 20; i++) {
+            jobs.addLast(new MockJob());
+            myCompJob.addJob(jobs.peekLast());
+        }
+
+        myCompJob.execute(monitor);
+
+        int executionNumber = 0;
+        while (!jobs.isEmpty()) {
+            final MockJob job = jobs.removeFirst();
+            assertTrue("Job was executed in the wrong order!", job.getExecutionNumber() > executionNumber);
+            executionNumber = job.getExecutionNumber();
+        }
+    }
+    
+    /**
+     * add a number of jobs. execute the composite job. After half of the jobs are executed, add some more. 
+     * check if all jobs were executed in the order they were added.
+     *
+     * @throws JobFailedException
+     *             the job failed exception
+     * @throws UserCanceledException
+     *             the user canceled exception
+     */
+    @Test
+    public void testInOrderExecutionWithDynamicExtension() throws JobFailedException, UserCanceledException {
+        final LinkedList<MockJob> jobs = new LinkedList<MockJob>();
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+
+        for (int i = 0; i < 20; i++) {
+            jobs.addLast(new MockJob());
+            myCompJob.addJob(jobs.peekLast());
+        }
+        myCompJob.add(new IJob() {            
+            @Override
+            public String getName() {
+                return "";
+            }
+            
+            @Override
+            public void execute(IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
+                for (int i = 0; i < 20; i++) {
+                    jobs.addLast(new MockJob());
+                    myCompJob.addJob(jobs.peekLast());
+                }
+            }
+            
+            @Override
+            public void cleanup(IProgressMonitor monitor) throws CleanupFailedException {
+            }
+        });
+
+        myCompJob.execute(monitor);
+
+        int executionNumber = 0;
+        while (!jobs.isEmpty()) {
+            final MockJob job = jobs.removeFirst();
+            assertTrue("Job was executed in the wrong order!", job.getExecutionNumber() > executionNumber);
+            executionNumber = job.getExecutionNumber();
+        }
+    }
+    
+    @Test
+    public void testJobAddedDuringCleanupIsNotExecuted() throws JobFailedException, UserCanceledException, CleanupFailedException {
+        final MockJob job = new MockJob();
+        myCompJob = new DynamicSequentialJob(false);
+        myCompJob.addJob(new IJob() {            
+            @Override
+            public String getName() {
+                return "";
+            }
+            
+            @Override
+            public void execute(IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
+            }
+            
+            @Override
+            public void cleanup(IProgressMonitor monitor) throws CleanupFailedException {
+                myCompJob.addJob(job);
+            }
+        });
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+
+        myCompJob.execute(monitor);
+        assertEquals(false, job.wasExecuted());
+
+        myCompJob.cleanup(monitor);
+        assertEquals(false, job.wasCleanedUp());
+    }
+
+    /**
+     * test a failed job.
+     *
+     * @throws JobFailedException
+     *             the job failed exception
+     * @throws UserCanceledException
+     *             the user canceled exception
+     */
+    @Test(expected=WorkflowFailedException.class)
+    public void testFailedJob() throws JobFailedException, UserCanceledException {
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+
+        myCompJob.addJob(new FailingJob());
+        myCompJob.execute(monitor);
+    }
+
+    /**
+     * test a canceled job.
+     *
+     * @throws JobFailedException
+     *             the job failed exception
+     * @throws UserCanceledException
+     *             the user canceled exception
+     */
+    @Test(expected=WorkflowFailedException.class)
+    public void testCanceledJob() throws JobFailedException, UserCanceledException {
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+
+        myCompJob.addJob(new CancelingJob());
+        myCompJob.execute(monitor);
+    }
+}
---
+++ tests/de.uka.ipd.sdq.workflow.tests/src/de/uka/ipd/sdq/workflow/tests/JobProxyTests.java
@@ -0,0 +1,86 @@
+package de.uka.ipd.sdq.workflow.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.function.Supplier;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import de.uka.ipd.sdq.workflow.jobs.CleanupFailedException;
+import de.uka.ipd.sdq.workflow.jobs.DynamicSequentialJob;
+import de.uka.ipd.sdq.workflow.jobs.IJob;
+import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
+import de.uka.ipd.sdq.workflow.jobs.JobProxy;
+import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
+import de.uka.ipd.sdq.workflow.mocks.CancelingJob;
+import de.uka.ipd.sdq.workflow.mocks.MockJob;
+
+/**
+ * The Class JobProxyTests.
+ */
+public class JobProxyTests {
+
+    @Before
+    public void setUp() {
+        MockJob.resetExecutionNumber();
+    }
+    
+    @Test
+    public void testJobHandling() throws JobFailedException, UserCanceledException, CleanupFailedException {
+        final MockJob job = new MockJob();
+        var proxy = new JobProxy(() -> job);
+        
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+        proxy.execute(monitor);
+        assertEquals(true, job.wasExecuted());
+        proxy.cleanup(monitor);
+        assertEquals(true, job.wasCleanedUp());
+    }
+    
+    private boolean supplierExecuted = false;
+    @Test
+    public void testDelayedInit() throws JobFailedException, UserCanceledException, CleanupFailedException {
+        final MockJob job = new MockJob();
+        Supplier<IJob> trackingSupplier = () -> {
+            supplierExecuted = true;
+            return job;
+        };
+        var proxy = new JobProxy(trackingSupplier);
+        
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+        assertEquals(false, supplierExecuted);
+        proxy.execute(monitor);
+        assertEquals(true, supplierExecuted);
+        assertEquals(true, job.wasExecuted());
+    }
+    
+    /**
+     * test a not executed job
+     * @throws CleanupFailedException 
+     */
+    @Test(expected=CleanupFailedException.class)
+    public void testNotExecutedJob() throws CleanupFailedException {
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+        final MockJob job = new MockJob();
+        var proxy = new JobProxy(() -> job);
+        proxy.cleanup(monitor);
+    }
+
+
+    /**
+     * test a canceled job.
+     *
+     * @throws JobFailedException
+     *             the job failed exception
+     * @throws UserCanceledException
+     *             the user canceled exception
+     */
+    @Test(expected=UserCanceledException.class)
+    public void testCanceledJob() throws JobFailedException, UserCanceledException {
+        final NullProgressMonitor monitor = new NullProgressMonitor();
+        var proxy = new JobProxy(() -> new CancelingJob());
+        proxy.execute(monitor);
+    }
+}

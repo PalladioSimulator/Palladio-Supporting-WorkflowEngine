3cd390a7a4f3f5c54afae940d8bf80f5661d1afc
Ralf Sieger <sieger@fzi.de>
2024-11-28 08:46:59 +0100

    moved constant definition to non UI plugin

---
+++ bundles/de.uka.ipd.sdq.workflow.launchconfig.core/src/main/java/de/uka/ipd/sdq/workflow/launchconfig/core/DebugConfigConstants.java
@@ -0,0 +1,8 @@
+package de.uka.ipd.sdq.workflow.launchconfig.core;
+
+public class DebugConfigConstants {
+
+    /** The Constant WORKFLOW_ENGINE_DEBUG_LEVEL. */
+    public static final String WORKFLOW_ENGINE_DEBUG_LEVEL = "de.uka.ipd.sdq.workflowengine.debuglevel";
+
+}

35ae7fac72e488383bd8c05816d1e39f0d619d3b
Ralf Sieger <sieger@fzi.de>
2024-11-29 09:35:10 +0100

    separated UI from backboard configuration

--- bundles/de.uka.ipd.sdq.workflow.mdsd/src/de/uka/ipd/sdq/workflow/mdsd/AbstractWorkflowBasedMDSDLaunchConfigurationDelegate.java
+++ bundles/de.uka.ipd.sdq.workflow.mdsd/src/de/uka/ipd/sdq/workflow/mdsd/AbstractWorkflowBasedMDSDLaunchConfigurationDelegate.java
@@ -1,16 +1,10 @@
 package de.uka.ipd.sdq.workflow.mdsd;
 
-import java.util.List;
-
-import org.apache.log4j.Level;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 
-import de.uka.ipd.sdq.workflow.BlackboardBasedWorkflow;
-import de.uka.ipd.sdq.workflow.launchconfig.core.AbstractWorkflowBasedLaunchConfigurationDelegate;
 import de.uka.ipd.sdq.workflow.launchconfig.core.AbstractWorkflowBasedRunConfiguration;
-import de.uka.ipd.sdq.workflow.logging.console.LoggerAppenderStruct;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 import de.uka.ipd.sdq.workflow.mdsd.blackboard.ResourceSetPartition;
 import de.uka.ipd.sdq.workflow.ui.UIBasedWorkflow;
@@ -26,17 +20,7 @@ import de.uka.ipd.sdq.workflow.ui.UIBasedWorkflow;
  * @author Steffen Becker
  */
 public abstract class AbstractWorkflowBasedMDSDLaunchConfigurationDelegate<WorkflowConfigurationType extends AbstractWorkflowBasedRunConfiguration>
-        extends
-        AbstractWorkflowBasedLaunchConfigurationDelegate<WorkflowConfigurationType, BlackboardBasedWorkflow<MDSDBlackboard>> {
-
-    /**
-     * Factory method for the blackboard used in the workflow of this launch.
-     *
-     * @return The blackboard to be used in the workflow
-     */
-    protected MDSDBlackboard createBlackboard() {
-        return new MDSDBlackboard();
-    }
+        extends AbstractWorkflowBasedMDSDBlackboardLaunchConfigurationDelegate<WorkflowConfigurationType> {
 
     /*
      * (non-Javadoc)
@@ -52,19 +36,4 @@ public abstract class AbstractWorkflowBasedMDSDLaunchConfigurationDelegate<Workf
                 this.createExceptionHandler(workflowConfiguration.isInteractive()), this.createBlackboard());
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see de.uka.ipd.sdq.workflow.launchconfig.AbstractWorkflowBasedLaunchConfigurationDelegate#
-     * setupLogging(org.apache.log4j.Level)
-     */
-    @Override
-    protected List<LoggerAppenderStruct> setupLogging(final Level logLevel) throws CoreException {
-        List<LoggerAppenderStruct> loggerList = super.setupLogging(logLevel);
-
-        // Setup openArchitectureWare Logging
-        loggerList.add(this.setupLogger("org.openarchitectureware", logLevel, SHORT_LOG_PATTERN));
-
-        return loggerList;
-    }
 }

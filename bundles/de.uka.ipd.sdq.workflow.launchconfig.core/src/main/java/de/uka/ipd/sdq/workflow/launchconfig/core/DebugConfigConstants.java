8e2a9a01c72998668b13b81608b65df3f26d7bf2
rsfzi <147811838+rsfzi@users.noreply.github.com>
2024-12-02 19:49:06 +0100

    Revert "moved constant definition to non UI plugin"
    This reverts commit 3cd390a7a4f3f5c54afae940d8bf80f5661d1afc.

--- bundles/de.uka.ipd.sdq.workflow.launchconfig.core/src/main/java/de/uka/ipd/sdq/workflow/launchconfig/core/DebugConfigConstants.java
+++
@@ -1,8 +0,0 @@
-package de.uka.ipd.sdq.workflow.launchconfig.core;
-
-public class DebugConfigConstants {
-
-    /** The Constant WORKFLOW_ENGINE_DEBUG_LEVEL. */
-    public static final String WORKFLOW_ENGINE_DEBUG_LEVEL = "de.uka.ipd.sdq.workflowengine.debuglevel";
-
-}
